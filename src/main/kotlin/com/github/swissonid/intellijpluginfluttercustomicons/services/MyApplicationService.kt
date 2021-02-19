424130f224e4399708a2d7a12dda7ef4dd8c9635
GitHub Action <action@github.com>
2021-02-19 16:14:53 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/swissonid/intellijpluginfluttercustomicons/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.swissonid.intellijpluginfluttercustomicons.services
+
+import com.github.swissonid.intellijpluginfluttercustomicons.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/swissonid/intellijpluginfluttercustomicons/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swissonid/intellijpluginfluttercustomicons/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swissonid.intellijpluginfluttercustomicons.services
 
+import com.github.swissonid.intellijpluginfluttercustomicons.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
